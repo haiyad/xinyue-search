eaa3c047533c2a879c2395acf675d667f2588d99
oneloves <1686618@qq.com>
2026-03-04 20:35:38 +0800

    chore: 清理缓存文件和临时文件
    删除过期的缓存文件和临时文件，包括runtime1/cache目录下的多个php缓存文件、qfadmin/log日志文件、api/cache排名数据缓存文件以及qfadmin/temp临时模板文件

--- runtime1/cache/df/0f2f8b8a6606196a052ee2579ecb0a.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000060
- exit();?>
-a:2:{s:5:"title";s:3:"731";s:3:"url";s:35:"https://pan.quark.cn/s/a21e8358d1e0";}
--- runtime1/cache/fd/005c4fcf41cf7d9a7dee3e0f5b528e.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000060
- exit();?>
-a:2:{s:5:"title";s:14:"仙逆(臻彩)";s:3:"url";s:35:"https://pan.quark.cn/s/7f2950ca3dff";}
